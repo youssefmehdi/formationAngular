1504847190a8c00e9c646bb1f921baeaaba7e681
mehdi <youssef@msakni.com>
2018-04-03 15:02:01 +0200

    add shared /core modules

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
+  imports: [
+    CoreModule,
+    BrowserModule,
+    SharedModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
