f31d5c2d250c02ef62b28bf0540aba6d3f5f16a9
mehdi <youssef@msakni.com>
2018-04-04 11:52:12 +0200

    refactoring of item

---
+++ my-app/src/app/shared/components/item/item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Item } from '../../models/item.model';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+  @Input() item: Item;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- my-app/src/app/shared/shared.module.ts
+++ my-app/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './components/nav/nav.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavComponent],
-  exports: [NavComponent]
+  declarations: [NavComponent, ItemComponent],
+  exports: [NavComponent, ItemComponent]
 })
 export class SharedModule { }
